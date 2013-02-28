2f6572d1afa8a768e8459d0083368e25e64e48f8
Pietari Kettunen <kettunen@gmail.com>
2013-03-01 00:21:20 +0100

    refactored char printing to separate map

--- src/main/java/ch/piet/Dojo.java
+++ src/main/java/ch/piet/Dojo.java
@@ -10,7 +10,7 @@ public class Dojo {
 	
 	public String printInAscii(int i) {
 		for (char ch : parseInput(i)) {
-			processChar(ch);
+			buildDisplayLines(NumbersAsAscii.getNumberCharAsAsciiStringArray(ch));
 		}
 		return top + EOL + middle + EOL + bottom;
 	}
@@ -23,100 +23,9 @@ public class Dojo {
 		return input.toCharArray();
 	}
 
-	private void processChar(char ch) {
-		switch (ch) {
-		case '0':
-			print0();
-			break;
-		case '1':
-			print1();
-			break;
-		case '2':
-			print2();
-			break;
-		case '3':
-			print3();
-			break;
-		case '4':
-			print4();
-			break;
-		case '5':
-			print5();
-			break;
-		case '6':
-			print6();
-			break;
-		case '7':
-			print7();
-			break;
-		case '8':
-			print8();
-			break;
-		case '9':
-			print9();
-			break;
-		default:
-			throw new RuntimeException("Not a number");
-		}
-	}
-
-	private void print9() {
-		top    += " _ ";
-		middle += "|_|";
-		bottom += " _|";
-	}
-
-	private void print8() {
-		top    += " _ ";
-		middle += "|_|";
-		bottom += "|_|";
-	}
-
-	private void print7() {
-		top    += " _ ";
-		middle += "  |";
-		bottom += "  |";
-	}
-
-	private void print6() {
-		top    += " _ ";
-		middle += "|_ ";
-		bottom += "|_|";
-	}
-
-	private void print5() {
-		top    += " _ ";
-		middle += "|_ ";
-		bottom += " _|";
-	}
-
-	private void print4() {
-		top    += "   ";
-		middle += "|_|";
-		bottom += "  |";
-	}
-
-	private void print3() {
-		top    += " _ ";
-		middle += " _|";
-		bottom += " _|";
-	}
-
-	private void print2() {
-		top    += " _ ";
-		middle += " _|";
-		bottom += "|_ ";
-	}
-
-	private void print1() {
-		top    += "   ";
-		middle += "  |";
-		bottom += "  |";
-	}
-
-	private void print0() {
-		top    += " _ ";
-		middle += "| |";
-		bottom += "|_|";
+	private void buildDisplayLines(String[] ascii) {
+		top    += ascii[0];
+		middle += ascii[1];
+		bottom += ascii[2];
 	}
 }
---
+++ src/main/java/ch/piet/NumbersAsAscii.java
@@ -0,0 +1,59 @@
+package ch.piet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NumbersAsAscii {
+
+	private static final Map<Character, String[]> numbers = new HashMap<>();
+	
+	static {
+		put('0', " _ ",
+				 "| |",
+				 "|_|");
+		
+		put('1', "   ",
+				 "  |",
+				 "  |");
+		
+		put('2', " _ ",
+				 " _|",
+				 "|_ ");
+		
+		put('3', " _ ",
+				 " _|",
+				 " _|");
+		
+		put('4', "   ",
+				 "|_|",
+				 "  |");
+		
+		put('5', " _ ",
+				 "|_ ",
+				 " _|");
+		
+		put('6', " _ ",
+				 "|_ ", 
+				 "|_|");
+		
+		put('7', " _ ",
+				 "  |",
+				 "  |");
+		
+		put('8', " _ ", 
+				 "|_|",
+				 "|_|");
+		
+		put('9', " _ ",
+				 "|_|",
+				 " _|");
+	}
+	
+	public static String[] getNumberCharAsAsciiStringArray(Character number) {
+		return numbers.get(number);
+	}
+	
+	private static void put(Character key, String row0, String row1, String row2) {
+		numbers.put(key, new String[] {row0, row1, row2});
+	}
+}
